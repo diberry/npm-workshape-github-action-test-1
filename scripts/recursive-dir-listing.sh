dfc08a6c2dd6112b21d4ea6888938c35c216c3f2
Dina Berry <41597107+diberry@users.noreply.github.com>
2021-05-17 13:53:39 -0700

    fix bash script - ignore hidden files

--- scripts/recursive-dir-listing.sh
+++ scripts/recursive-dir-listing.sh
@@ -1,1 +1,2 @@
-find . | sed -e "s/[^-][^\/]*\//  |/g" -e "s/|\([^ ]\)/|-\1/" 
+# ignore all hidden files and node_modules directory files
+find . ! -path '*/\.*' ! -path 'node_modules/*' | sed -e "s/[^-][^\/]*\//  |/g" -e "s/|\([^ ]\)/|-\1/" 
