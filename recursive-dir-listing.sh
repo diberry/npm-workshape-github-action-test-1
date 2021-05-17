e3bd8c7b0124add5b9f5d5ba38f3d83d273a2e51
Dina Berry <41597107+diberry@users.noreply.github.com>
2021-05-17 14:12:37 -0700

    edits

--- recursive-dir-listing.sh
+++ recursive-dir-listing.sh
@@ -1,2 +1,3 @@
 # ignore all hidden files and node_modules directory files
+echo "find all but ignore hidden and node_modules"
 find . ! -path '*/\.*' ! -path 'node_modules/*' | sed -e "s/[^-][^\/]*\//  |/g" -e "s/|\([^ ]\)/|-\1/" 
--- scripts/recursive-dir-listing.sh
+++
@@ -1,2 +0,0 @@
-# ignore all hidden files and node_modules directory files
-find . ! -path '*/\.*' ! -path 'node_modules/*' | sed -e "s/[^-][^\/]*\//  |/g" -e "s/|\([^ ]\)/|-\1/" 
